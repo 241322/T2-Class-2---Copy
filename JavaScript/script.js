93b2c6843864ae9dea1f0d8fe42ceece9fb75790
xanderMMV <241322@virtualwindow.co.za>
2024-04-30 12:56:33 +0200

    first basic js

---
+++ JavaScript/script.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.log("I am " + 19 + " years old");
+console.log(60+9);
+
+let ticketNum = 3;
+const pi = 3.142;
+
+let firstName = "Xander", surname = "Poalses", age = 19;
